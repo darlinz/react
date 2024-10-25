5a75a0cabcab8e7c80afaab960f4dd805ed368c8
Darina Krostsenko <132041028+darlinz@users.noreply.github.com>
2024-10-25 13:27:39 +0300

    Add files via upload

--- React_project_1/src/components/search-panel/search-panel.js
+++ React_project_1/src/components/search-panel/search-panel.js
@@ -10,4 +10,20 @@ export default class SearchPanel extends Component {
         }
         this.onUpdateSearch = this.onUpdateSearch.bind(this);
     }
+    onUpdateSearch(e){
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+    render() {
+        return (
+            <input
+                className="form-control search-input"
+                type="text"
+                placeholder="Поиск по записям"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
 }
