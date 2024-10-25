48106d58eccc9f33f04eec13a8930092142e1212
Darina Krostsenko <132041028+darlinz@users.noreply.github.com>
2024-10-25 12:32:44 +0300

    Add files via upload

---
+++ React_project_1/src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+import './search-panel.css'
+
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    }
+}
