7fec37b0c88078cf32634fda9d53c4d189cd51c5
Darina Krostsenko <132041028+darlinz@users.noreply.github.com>
2024-10-25 08:26:47 +0300

    Add files via upload

--- react/react_1/src/index.js
+++ react/react_1/src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/app/';
+
+ReactDOM.render(<App />, document.getElementById('root'));
